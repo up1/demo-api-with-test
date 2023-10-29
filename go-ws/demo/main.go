35e7ef3f318810add656322f815f72d27c0b44d4
up1 <somkiat.p@gmail.com>
2023-10-29 16:04:13 +0700

    Go workspace

---
+++ go-ws/demo/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"s1"
+	"s2"
+)
+
+func main() {
+	fmt.Println(s1.SayHi())
+	fmt.Println(s2.SayHi())
+}
---
+++ go-ws/s2/s2.go
@@ -0,0 +1,5 @@
+package s2
+
+func SayHi() string {
+	return "Hi from S2"
+}
