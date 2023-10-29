2c5f8bc5ed6699c16a0932fe167fc5fa5686ef16
up1 <somkiat.p@gmail.com>
2023-10-29 15:43:11 +0700

    Create first test

---
+++ go/api_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+func TestHelloWorld(t *testing.T) {
+	// if HelloWorld() != "Hello World!" {
+	// 	t.Error("Test failed")
+	// }
+}
+
+func BenchmarkXXX(b *testing.B) {
+	// if HelloWorld() != "Hello World!" {
+	// 	t.Error("Test failed")
+	// }
+}
